20fdc0c19c96880fc80ccfab34976296bd96af4b
CharlieB <qfc@msu.edu>
2020-07-01 15:42:40 -0400

    Added example of saving a plot as an image with a specific size

---
+++ scripts-extensions/setOutputSize.R
@@ -0,0 +1,26 @@
+{
+  source(file="scripts/reference.R");  # this line will be in all your scripts
+  weatherData = read.csv(file="data/LansingNOAA2016.csv", 
+                         stringsAsFactors = FALSE);
+
+  #### Part 5: Add labels and titles ####
+  plot1 = ggplot(data=weatherData) +
+          geom_point(mapping=aes(x=avgTemp, y=relHum),
+                     color="darkgreen", 
+                     size=2.5, 
+                     shape=17, 
+                     alpha = 0.4 ) +
+          theme_bw() +
+          labs(title = "Humidity vs. Temperature",
+               subtitle = "Lansing, Michigan: 2016",
+               x = "Temperature (F)",
+               y = "Humidity (%)");
+  plot(plot1);
+
+  ggsave(filename = "images/sizedPlot.png",  
+         plot = plot1,
+         width = 12,
+         height = 10,
+         units = "cm");   # can also use "in" or "mm"
+
+}  
