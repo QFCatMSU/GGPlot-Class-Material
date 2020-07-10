2b031d6a940940506c1002e913b23dd5da53eee5
CharlieB <qfc@msu.edu>
2020-07-10 09:35:41 -0400

    Added extra examples for app2 and app4

---
+++ application_answers/app02-remove trailing zeros.R
@@ -0,0 +1,17 @@
+{
+  # read in the lines of code from helper.r
+  source(file="scripts/reference.r"); 
+
+  # read in CSV file and save the content to packageData
+  packageData = read.csv(file="data/accdeaths.csv");
+  
+  plotData = ggplot( data=packageData ) +
+    geom_point( mapping=aes(x=time, y=value) ) +
+    ggtitle( label="Accidental deaths in US 1973-1978" ) +
+    # labels "renames" the axis tick values and for unknown reasons removes the trailing zeros
+    scale_x_continuous( breaks = seq(from=1973, to=1978, by=.5),
+                        labels = seq(from=1973, to=1978, by=.5)) +
+    scale_y_continuous( breaks = seq(from=5000, to=15000, by=2000) ) +
+    theme( axis.text.x=element_text(angle=45, hjust=1) );
+  plot(plotData);
+}
