0f845f79ec0bf51f6c2ccd009ac4d8b23a23a605
CharlieB <qfc@msu.edu>
2020-08-03 12:12:44 -0400

    8/3 class material uploaded -- small edits

---
+++ in-class/8-3 Class Material/For loops and state variables Answers.R
@@ -0,0 +1,135 @@
+{
+  source(file="scripts/reference.R");  
+  weatherData = read.csv(file="data/LansingNOAA2016-3.csv", 
+                         stringsAsFactors = FALSE);
+ 
+  ### Replace "T" in precip column with 0.005 using gsub()
+  weatherData$precip2 = gsub(x=weatherData$precip, 
+                             pattern="T", 
+                             replacement="0.005");
+  
+  ### Probably want the column to be numeric...
+  weatherData$precip3 = as.numeric(gsub(x=weatherData$precip, 
+                             pattern="T", 
+                             replacement="0.005"));
+  
+  ### Plot humidity vs. precip2 (precip2 is string value -- this will cause problems!) 
+  thePlot = ggplot(data=weatherData) +
+    geom_point(mapping=aes(x=relHum, y=precip2)) +
+    theme_bw() +
+    labs(title = "Relative Humidity vs. Precipitation",
+         subtitle = "Lansing, Michigan: 2016",
+         x = "Humidity",
+         y = "Precipitation (string/character/factor/categorical values)");
+  plot(thePlot);
+
+
+  ### Plot humidity vs. precip3 (much better)  
+  thePlot = ggplot(data=weatherData) +
+    geom_point(mapping=aes(x=relHum, y=precip3)) +
+    theme_bw() +
+    labs(title = "Relative Humidity vs. Precipitation",
+         subtitle = "Lansing, Michigan: 2016",
+         x = "Humidity",
+         y = "Precipitation (numeric values)");
+  plot(thePlot);
+
+  # Find which month has the most rain (take one)
+  precipEachMonth = c(rep(0,12));   # repeat zero 12 times, so c(0,0,0,0,0,0,0,0,0,0,0,0)
+  
+  for(i in 1:length(weatherData$precip3))  # go through each rwo (day)
+  {
+    # check each month and add the precip to the correct month
+    if(grepl(x=weatherData$date[i], pattern="01-")) 
+    {
+      precipEachMonth[1] = precipEachMonth[1] + weatherData$precip3[i];
+    }
+    else if(grepl(x=weatherData$date[i], pattern="02-")) 
+    {
+      precipEachMonth[2] = precipEachMonth[2] + weatherData$precip3[i]; 
+    }
+    else if(grepl(x=weatherData$date[i], pattern="03-")) 
+    {
+      precipEachMonth[3] = precipEachMonth[3] + weatherData$precip3[i]; 
+    }
+    else if(grepl(x=weatherData$date[i], pattern="04-")) 
+    {
+      precipEachMonth[4] = precipEachMonth[4] + weatherData$precip3[i]; 
+    }
+    else if(grepl(x=weatherData$date[i], pattern="05-")) 
+    {
+      precipEachMonth[5] = precipEachMonth[5] + weatherData$precip3[i]; 
+    }
+    else if(grepl(x=weatherData$date[i], pattern="06-")) 
+    {
+      precipEachMonth[6] = precipEachMonth[6] + weatherData$precip3[i]; 
+    }
+    else if(grepl(x=weatherData$date[i], pattern="07-")) 
+    {
+      precipEachMonth[7] = precipEachMonth[7] + weatherData$precip3[i]; 
+    }
+    else if(grepl(x=weatherData$date[i], pattern="08-")) 
+    {
+      precipEachMonth[8] = precipEachMonth[8] + weatherData$precip3[i]; 
+    }
+    else if(grepl(x=weatherData$date[i], pattern="09-")) 
+    {
+      precipEachMonth[9] = precipEachMonth[9] + weatherData$precip3[i]; 
+    }
+    else if(grepl(x=weatherData$date[i], pattern="10-")) 
+    {
+      precipEachMonth[10] = precipEachMonth[10] + weatherData$precip3[i]; 
+    }
+    else if(grepl(x=weatherData$date[i], pattern="11-")) 
+    {
+      precipEachMonth[11] = precipEachMonth[11] + weatherData$precip3[i]; 
+    }
+    else if(grepl(x=weatherData$date[i], pattern="12-")) 
+    {
+      precipEachMonth[12] = precipEachMonth[12] + weatherData$precip3[i]; 
+    }
+  }
+  cat("\nTake 1: Month with most rain is month:", which.max(precipEachMonth),
+      "\n        Amount of rain that month was:", max(precipEachMonth));
+  
+  ### Take 2: Replace the if-else with a for loop
+  precipEachMonth2 = c(rep(0,12));
+  # need to put the month patterns in a vectors
+  month = c("01-", "02-", "03-", "04-", "05-", "06-",
+            "07-", "08-", "09-", "10-", "11-", "12-");
+  
+  for(i in 1:length(weatherData$precip3))   # go through each row
+  {
+    for(j in 1:length(month))               # go through each month
+    {
+      # if its the correct month -- add the value
+      if(grepl(x=weatherData$date[i], pattern=month[j])) 
+      {
+        precipEachMonth2[j] = precipEachMonth2[j] + weatherData$precip3[i];
+        break;   # saves processing time but you can take it out
+      }
+    }
+  }
+  cat("\nTake 2: Month with most rain is month:", which.max(precipEachMonth2),
+      "\n        Amount of rain that month was:", max(precipEachMonth2));
+  
+  
+  ### Take 3: Replace the month pattern vector
+  precipEachMonth3 = c(rep(0,12));
+
+  for(i in 1:length(weatherData$precip3))
+  {
+    for(j in 1:12)
+    {
+      twoDigitMonth = formatC(j, width=2, flag="0");        # 01, 02...
+      if(grepl(x=weatherData$date[i], 
+               pattern=paste(twoDigitMonth, "-", sep="")))  # 01-, 02-, ...
+      {
+        precipEachMonth3[j] = precipEachMonth3[j] + weatherData$precip3[i];
+        break;  
+      }
+    }
+  }
+  cat("\nTake 3: Month with most rain is month:", which.max(precipEachMonth3),
+      "\n        Amount of rain that month was:", max(precipEachMonth3));
+}  
---
+++ in-class/8-3 Class Material/app08- assignment (for and geoms).R
@@ -0,0 +1,129 @@
+{
+  source( file="scripts/reference.R" ); 
+  library(reshape);
+  weatherData = read.csv( file="data/LansingNOAA2016-3.csv", 
+                          stringsAsFactors = FALSE );
+  
+  # format the dateYr column as a date with just the abbreviated month (%b)
+  months = format.Date(weatherData$dateYr, format="%b");
+  weatherData$month = months;      # save months to data frame as new column
+  
+  ###############################################
+  ##### This is the section you will replace ####
+  ###############################################
+  
+  # create the heatDays and CoolDays dataframes
+  heatDayList = aggregate(formula=coolDays~month, 
+                          data=weatherData, 
+                          FUN=sum);
+  coolDayList = aggregate(formula=heatDays~month, 
+                          data=weatherData, 
+                          FUN=sum);
+  
+  # merge the two data frame together
+  heatCoolByMonth = merge(heatDayList, coolDayList);
+  
+  # melt the dataframe so it is more GGPlot-friendly
+  meltedDF = melt(heatCoolByMonth, id="month");
+  
+  # Instructions to replace this section:
+  #   1) Create three vectors with 12 values each
+  #    - one to hold the 12 abbreviated months (Jan, Feb...)
+  #    - one to hold the total heatDays for each month
+  #    - one to hold the total coolDays for each month
+  # 
+  #   2) Using for loops (the number of for loops differs based on your strategy...)
+  #    - sum up the heatDays and the coolDays for each month
+  # 
+  #   3) Combine the three vectors into the heatCoolByMonth dataframe
+  #    - make sure you name the columns appropriately
+  #    
+  #   4) Challenge: Create a melted dataframe from the three vectors (there will be 24 rows)
+  #    - first column has the 12 months repeated twice
+  #    - second column has the value "heatDays" 12 times and "coolDays" 12 times
+  #    - third column has the summed vales for heatDays and coolDays based on month 
+  #    - call the dataframe meltedDF and name the columns appropriately 
+  #
+  #   5) Extra: add an unofficial legend to the geom_col() plots using annotate()
+  #    - the options for geom are: segment, rect, and text
+  
+  ###################################
+  #### End of section to replace ####
+  ###################################
+  
+  #### Four different plots -- same output
+  # 1) geom_col() with regular data frame
+  # 2) geom_col() with melted data frame
+  # 3) geom_bar() with regular data frame
+  # 4) geom_bar() with melted data frame
+  
+  # geom_col method -- unmelted dataframe
+  thePlot = ggplot(heatCoolByMonth) +
+    # plot the two column separately, nudge them so they are not stacked
+    geom_col(mapping=aes(x=month, y=heatDays),
+             width=0.4,
+             fill= "red",
+             position=position_nudge(x=-0.2))  +
+    geom_col(mapping=aes(x=month, y=coolDays),
+             width=0.4,
+             fill= "blue",
+             position=position_nudge(x=0.2))  +    
+    scale_x_discrete(limits = month.abb) +
+    theme_bw() +
+    labs(title = "Heating and Cooling Days",
+         subtitle = "geom_col() -- unmelted dataframe",
+         x = "Month",
+         y = "Cumulative Heat/Cool Days");
+  plot(thePlot);
+  
+  # geom_col method -- melted dataframe
+  thePlot = ggplot(meltedDF) +
+    # plot values and fill with variable, use dodge so they do not stack 
+    geom_col(mapping=aes(x=month, y=value, fill=variable),
+             width=0.4,
+             position=position_dodge())  +
+    scale_x_discrete(limits = month.abb) +
+    theme_bw() +
+    labs(title = "Heating and Cooling Days",
+         subtitle = "geom_col() -- melted dataframe",
+         x = "Month",
+         y = "Cumulative Heat/Cool Days");
+  plot(thePlot);
+  
+  # geom_bar method -- unmelted dataframe
+  thePlot = ggplot(data=heatCoolByMonth) +
+    geom_bar(mapping=aes(x=month, y=heatDays),
+             stat="identity",  # without this, geom_bar does a count 
+             width=0.4, 
+             fill="red",
+             position = position_nudge(x=0.2)) + 
+    geom_bar(mapping=aes(x=month, y=coolDays),
+             stat="identity", 
+             width=0.4, 
+             fill="blue",
+             position = position_nudge(x=-0.2)) +  
+    scale_x_discrete(limits = month.abb) +
+    scale_fill_manual(values=c("red", "blue")) +
+    theme_bw() +
+    labs(title = "Heating and Cooling Days",
+         subtitle = "geom_bar() -- unmelted dataframe",
+         x = "Month",
+         y = "Cumulative Heat/Cool Days");
+  plot(thePlot);
+  
+  # geom_bar method -- melted dataframe
+  thePlot = ggplot(data=meltedDF) +
+    geom_bar(mapping=aes(x=month, y=value, fill=variable),
+             stat="identity",  # without this, it will do a count (on X or Y)
+             width=0.5, 
+             position=position_dodge()) + # without this, it will stack
+    scale_x_discrete(limits = month.abb) +
+    scale_fill_manual(values=c("red", "blue")) +
+    theme_bw() +
+    labs(title = "Heating and Cooling Days",
+         subtitle = "geom_bar() -- melted dataframe",
+         x = "Month",
+         y = "Cumulative Heat/Cool Days");
+  plot(thePlot);
+}
+
