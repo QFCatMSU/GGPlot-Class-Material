0c22add4bea489daf89a0d0236ea2732eb166120
CharlieB <qfc@msu.edu>
2020-08-26 13:00:26 -0400

    minor change

--- scripts/reference.r
+++ scripts/reference.r
@@ -1,7 +1,8 @@
 {
   rm(list=ls());                         # clear Console Window
   options(show.error.locations = TRUE);  # show line numbers on error
-  library(package=ggplot2);   library(package=gridExtra);           
+  library(package=ggplot2);   
+  library(package=gridExtra);           
   
   # used in extension for lesson 7 -- Boxplots2
   findQuants = function(yVar, xVar, factors, percentile)
