f45b7ecb7a38015300621899e49e37fbbb2f6d6c
Charlie <belinsky@msu.edue>
2020-11-30 09:05:51 -0500

    minor change

--- application_answers/app01.R
+++ application_answers/app01.R
@@ -1,4 +1,4 @@
-{
+{ 
   rm(list=ls());                         # clear Console Window
   options(show.error.locations = TRUE);  # show line numbers on error
   library(package=ggplot2);              # include all GGPlot2 functions
