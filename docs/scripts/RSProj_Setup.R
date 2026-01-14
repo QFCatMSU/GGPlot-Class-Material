48a8fca2f4f7d4d9fb4fcf98486da956aa540883
belinskyc <belinsky@msu.edu>
2026-01-14 11:59:48 -0500

    multiple changes to setup lessons

--- docs/scripts/RSProj_Setup.R
+++ docs/scripts/RSProj_Setup.R
@@ -1,5 +1,4 @@
 rm(list=ls());                         # clear Console Window
-options(show.error.locations = TRUE);  # show line numbers on error
 library(package=ggplot2);              # include all GGPlot2 functions
 
 weatherData = read.csv(file="data/RSProj_Setup.csv");
